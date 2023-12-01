2f099ac0019b1c7b1cee1a4f822a336fbc3179cc
Timo Caktu <timocaktu@gmail.com>
2023-12-01 10:04:45 +0100

    added readme file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod day01;
 
 fn main() {
-    println!("First Star Solution: {:?}", day01::take_numbers_of_vec_and_calc_sum());
-    println!("Second Star Solution: {:?}", day01::take_numbers_of_vec_and_calc_real_sum());
+    println!("day01: First Star Solution: {:?}", day01::take_numbers_of_vec_and_calc_sum());
+    println!("day01: Second Star Solution: {:?}", day01::take_numbers_of_vec_and_calc_real_sum());
 }
